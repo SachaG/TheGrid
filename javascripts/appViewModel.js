021b58d744ac63ffd16972dfaeca7083a3c3859c
Sacha Greif <sacha357@gmail.com>
2012-08-15 07:51:18 +0900

    Revert "Revert "working on knockout + require""
    This reverts commit f8a82f6be9c235c487428578c0f6850cd57c32bd.

--- javascripts/appViewModel.js
+++ javascripts/appViewModel.js
@@ -1,5 +1,4 @@
 define(['knockout-2.1.0', 'knockout.mapping', 'knockout.mustache'], function(ko, mapping, mustacheTemplateEngine) {
-	ko.setTemplateEngine(new mustacheTemplateEngine());
 
 	function addCustomObservables(index, post, parentArray){
 		post.getDomain = ko.computed(function(){
@@ -11,8 +10,13 @@ define(['knockout-2.1.0', 'knockout.mapping', 'knockout.mustache'], function(ko,
 		var self = this;
 		// viewModel = ko.observableArray(data);
 		viewModel= mapping.fromJS(data);
+		cLog(data);
+		// $.each(viewModel.items(), function(index, element){
+		// 	cLog(element.title());
+		// });
 		$.each(viewModel.items(), function(index, post){
 			addCustomObservables(index, post, viewModel.items());
 		});
+
 	}
 });
