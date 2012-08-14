55e3b0be3b9e53c7a01bef8642a4f06b7555d3e4
Sacha Greif <sacha357@gmail.com>
2012-08-15 07:51:18 +0900

    Revert "trying out knockout.mustache"
    This reverts commit a5183f00827c46b305849bed63364a91602d78e5.

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,44 +1,49 @@
 $("body, html").removeClass("no-js");
 
 function get_hostname(url) {
-	var m = ((url||'')+'').match(/^http:\/\/[^/]+/);
-	return m ? m[0].substr(7) : null;
+    var m = ((url||'')+'').match(/^http:\/\/[^/]+/);
+    return m ? m[0].substr(7) : null;
 }
 
 postsURL='http://api.ihackernews.com/page'+'?callback=?';
 postsURL='http://api.ihackernews.com/page';
 postsURL='load-json.php';
 postsURL='http://api.ihackernews.com/page?format=jsonp&callback=?';
-postsURL='posts.json';
+// postsURL='posts.json';
 
 require([
 	"jquery", 
-	"mustache",
 	"knockout-2.1.0", 
-	"knockout.mapping",
-	"knockout.mustache",
-	"appViewModel",
+	"knockout.mapping", 
 	"jquery.hotkeys", 
 	"plugins"
 ], function(
 	$, 
-	mustache,
 	ko, 
-	mapping,
-	mustacheTemplateEngine,
-	appViewModel
+	mapping
 ) {
 	//---------------------------------------- Knockout Stuff ---------------------------------------//
-
-
-		ko.mapping=mapping;
-
-
-
-		//---------------------------------------- Start Main jQuery Document Ready ---------------------------------------//
-		$(function() {
-			$.when($.getJSON(postsURL)).then(function(posts){
-				ko.applyBindings(new appViewModel(posts));	
-			});
+	ko.mapping=mapping;
+	
+	function addCustomObservables(index, post, parentArray){
+		post.getDomain = ko.computed(function(){
+			return get_hostname(post.url());
 		});
+	}
+	// Overall viewmodel for this screen, along with initial state
+	function ProjectsViewModel(data) {
+		var self = this;
+		// viewModel = ko.observableArray(data);
+		viewModel= ko.mapping.fromJS(data);
+		$.each(viewModel.items(), function(index, post){
+			addCustomObservables(index, post, viewModel.items());
+		});
+	}
+
+	//---------------------------------------- Start Main jQuery Document Ready ---------------------------------------//
+	$(function() {
+		$.when($.getJSON(postsURL)).then(function(posts){
+			ko.applyBindings(new ProjectsViewModel(posts));	
+		});
+	});
 });
