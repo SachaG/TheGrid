3b087c1ddb017551448fbe16b826b580ba8797df
Sacha Greif <sacha357@gmail.com>
2012-08-15 08:28:24 +0900

    pushing to phpfog

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,3 +1,5 @@
+debug=true;
+
 $("body, html").removeClass("no-js");
 
 function get_hostname(url) {
@@ -5,30 +7,57 @@ function get_hostname(url) {
     return m ? m[0].substr(7) : null;
 }
 
-postsURL='http://api.ihackernews.com/page'+'?callback=?';
-postsURL='http://api.ihackernews.com/page';
-postsURL='load-json.php';
+cLog=function(s){
+  if(debug){
+    console.log(s);
+  }
+};
+
 postsURL='http://api.ihackernews.com/page?format=jsonp&callback=?';
 // postsURL='posts.json';
 
+require.config({
+	paths: {
+		knockout: 			"libs/knockout-2.1.0",
+		mapping: 			"libs/knockout.mapping",
+		mustache: 			"libs/mustache"
+	}
+})
 require([
-	"jquery", 
-	"knockout-2.1.0", 
-	"knockout.mapping", 
-	"jquery.hotkeys", 
-	"plugins"
+	"jquery",
+	"mustache",
+	"knockout", 
+	"mapping"
 ], function(
-	$, 
+	$,
+	Mustache,
 	ko, 
 	mapping
 ) {
 	//---------------------------------------- Knockout Stuff ---------------------------------------//
 	ko.mapping=mapping;
 	
+	ko.mustacheTemplateEngine = function () { }
+	ko.mustacheTemplateEngine.prototype = ko.utils.extend(new ko.templateEngine(), {
+		renderTemplateSource: function (templateSource, bindingContext, options) {
+			var data = bindingContext.$data;
+			var templateText = templateSource.text();		
+			var htmlResult = Mustache.to_html(templateText, data);
+			
+			return ko.utils.parseHtmlFragment(htmlResult);
+		},
+		allowTemplateRewriting: false,
+		version: '0.9.0'
+	});
+	ko.setTemplateEngine(new ko.mustacheTemplateEngine());
+	
 	function addCustomObservables(index, post, parentArray){
 		post.getDomain = ko.computed(function(){
 			return get_hostname(post.url());
 		});
+		post.getIndex = ko.computed(function() {
+			return index+1;
+		})
 	}
 	// Overall viewmodel for this screen, along with initial state
 	function ProjectsViewModel(data) {
